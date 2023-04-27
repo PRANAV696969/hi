3b360274ef05ddef32c1d982cfabb81f8d6d6dfa
PRANAV696969 <107709470+PRANAV696969@users.noreply.github.com>
2023-04-27 09:48:16 +0530

    Update register.php

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
         <h1>Registration Form</h1>
         <form method="post" action="register.php">
                 <label for="username">Username:</label>
-                <input type="text" id="username" name="usernameee"><br><br>
+                <input type="text" id="username" name="usernameeeee"><br><br>
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email"><br><br>
                 <label for="password">Password:</label>
